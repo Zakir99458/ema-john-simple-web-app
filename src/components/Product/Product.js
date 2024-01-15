1d1781c34f2cbe3b3476b72ae990749a7bd898b0
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-01-15 11:02:17 +0200

    product-frm-api

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props.product);
+    const {name, img, price, stock, seller} = props.product;
+
+    return (
+        <div className='product'>
+            <div>
+                <img src= {img} alt="" />
+            </div>
+            <div>
+                <h4 className='product-name'>{name}</h4>
+                <p>by: <small>{seller}</small></p>
+                <p>price: {price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() =>{
+        fetch('./products.JSON')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    },[])
+
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h3>Products</h3>
+                <h3>Products: {products.length}</h3>
+                {
+                    products.map(product => <Product product={product}></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
